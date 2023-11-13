5b1521478413db7406cd3f76f10daa0984ea8f52
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2023-11-14 07:42:53 +0800

    1.add FluComboBox demo

---
+++ FluentUiDemo/FluComboBoxDemo.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <QWidget>
+#include "../FluentUiControl/FluComboBox.h"
+#include "../FluentUiUtils/FluentUiLogUtils.h"
+class FluComboBoxDemo : public QWidget
+{
+    Q_OBJECT
+  public:
+    FluComboBoxDemo(QWidget* widget = nullptr) : QWidget(widget)
+    {
+        FluComboBox* comboBox = new FluComboBox(this);
+        QList<QString> items = { " shoko 🥰 ", "西宫硝子 ", "aiko-----------", "柳井爱子"};
+        comboBox->addItems(items);
+        comboBox->setCurrentIndex(0);
+        connect(comboBox, &FluComboBox::currentTextChanged, [=](QString text) { LogDebug << "current Text:" << text; });
+        comboBox->move(200, 200);
+
+        resize(500, 500);
+        setStyleSheet("FluComboBoxDemo{background:white;}");
+    }
+};
