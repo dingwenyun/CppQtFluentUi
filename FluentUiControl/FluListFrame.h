c7c29cf8fcd81596623cb60a0589fed4fc5b5a7b
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2023-12-09 07:23:18 +0800

    1.FluentUi: FluListFrame -> FluViewInterface

--- FluentUiControl/FluListFrame.h
+++ FluentUiControl/FluListFrame.h
@@ -1,19 +1,33 @@
 #pragma once
 
 #include "FluFrame.h"
-#include "FluListView.h"
+#include "FluListWidget.h"
 
 class FluListFrame : public FluFrame
 {
   public:
     FluListFrame(QWidget* parent = nullptr) : FluFrame(parent)
     {
-        // m_listWidget = new FluListWidget(this);
-        // addWidget(m_listWidget)
-        // ...
+        m_listWidget = new FluListWidget(this);
+        m_hBoxLayout->addWidget(m_listWidget);
+
+        QList<QString> stands = {"白金之星", 
+            "法皇之绿", "天堂制造", "绯红之王", "银色战车",
+            "疯狂砖石", "金属制品", "败者食尘", "黑蚊子多", 
+            "杀手皇后", "壮烈成人", "石之自由", "砸瓦鲁多", 
+            "钢炼手指", "臭氧宝宝", "华丽挚爱", "隐者之紫",
+            "黄金体验", "虚无之王", "纸月之王", "骇人恶兽"
+        };
+
+        for (auto stand : stands)
+        {
+            auto item = new QListWidgetItem(stand);
+            m_listWidget->addItem(item);
+        }
+
+        setFixedSize(300, 380);
     }
 
   protected:
-    // mark it! wait for do it!
-    // FluListWidget* m_listWidget;
+    FluListWidget* m_listWidget;
 };
--- FluentUiControl/FluViewInterface.h
+++ FluentUiControl/FluViewInterface.h
@@ -2,6 +2,7 @@
 
 #include "FluGalleryInterface.h"
 #include "FluTableFrame.h"
+#include "FluListFrame.h"
 
 class FluViewInterface : public FluGalleryInterface
 {
@@ -10,6 +11,9 @@ class FluViewInterface : public FluGalleryInterface
     FluViewInterface(QWidget* parent = nullptr) : FluGalleryInterface("视图", "FluentUiControl::View", parent)
     {
         setObjectName("viewInterface");
+
+        auto listFrame = new FluListFrame(this);
+        addExampleCard("简单的列表控件", listFrame, "");
 
         auto taleFrame = new FluTableFrame(this);
         addExampleCard("简单的表格组件", taleFrame, "");
